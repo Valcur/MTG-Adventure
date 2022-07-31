5e52bcc77ee2f8d24db6913b4a05f635a3b1374d
Loic D <loic.danj@gmail.com>
2022-07-31 22:58:05 +0200

    Initial Commit

---
+++ MTG Singleplayer/MTG_SingleplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTG_SingleplayerApp.swift
+//  MTG Singleplayer
+//
+//  Created by Loic D on 31/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MTG_SingleplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
